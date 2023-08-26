272d68e0e4a59d548201be79f64565c02c476e29
RMIT-son <119044913+RMIT-son@users.noreply.github.com>
2023-08-26 18:35:43 +0700

    Make base variables for Vehicle class and make space for main file

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,10 @@
+import java.util.Collection;
+
 public class Vehicle {
+	protected String name;
+	protected String fuel;
+	protected double fuelCapacity;
+	protected double carryCapacity;
+	protected Port currentPort;
+	protected Collection<Container> containers;
 }
