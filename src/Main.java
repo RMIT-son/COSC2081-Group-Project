90b11e8a7ec8ca187d5da0867b5a7fd39223db3d
RMIT-son <119044913+RMIT-son@users.noreply.github.com>
2023-08-28 15:39:50 +0700

    Create constructor, setters, and getters

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
       // Main Program Code goes here
+
     }
 }
--- src/Trip.java
+++ src/Trip.java
@@ -7,4 +7,61 @@ public class Trip {
 	protected Port departurePort;
 	protected Port arrivalPort;
 	protected boolean status;
+
+	public Trip(Vehicle vehicle, LocalDate departureDate, LocalDate arrivalDate, Port departurePort, Port arrivalPort, boolean status) {
+		this.vehicle = vehicle;
+		this.departureDate = departureDate;
+		this.arrivalDate = arrivalDate;
+		this.departurePort = departurePort;
+		this.arrivalPort = arrivalPort;
+		this.status = status;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public LocalDate getDepartureDate() {
+		return departureDate;
+	}
+
+	public void setDepartureDate(LocalDate departureDate) {
+		this.departureDate = departureDate;
+	}
+
+	public LocalDate getArrivalDate() {
+		return arrivalDate;
+	}
+
+	public void setArrivalDate(LocalDate arrivalDate) {
+		this.arrivalDate = arrivalDate;
+	}
+
+	public Port getDeparturePort() {
+		return departurePort;
+	}
+
+	public void setDeparturePort(Port departurePort) {
+		this.departurePort = departurePort;
+	}
+
+	public Port getArrivalPort() {
+		return arrivalPort;
+	}
+
+	public void setArrivalPort(Port arrivalPort) {
+		this.arrivalPort = arrivalPort;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
 }
