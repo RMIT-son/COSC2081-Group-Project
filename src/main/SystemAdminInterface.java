ba5832855bb9aa1a641f751acd0d20f61ee1b3c0
liamvalliere <119045118+liamvalliere@users.noreply.github.com>
2023-09-15 11:39:35 +0700

    update sub class + user

---
+++ src/main/SystemAdminInterface.java
@@ -0,0 +1,10 @@
+package main;
+
+public class SystemAdminInterface {
+    public static void show() {
+        System.out.println("System Admin Menu:");
+        System.out.println("1. Access all port data");
+        System.out.println("2. Manage users");
+        // ... handle menu options here
+    }
+}
