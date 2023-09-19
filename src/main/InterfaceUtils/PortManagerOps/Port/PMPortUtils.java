a9cba554e26e5ab85bf83411b4aa6ae3320c1ed5
RMIT-son <119044913+RMIT-son@users.noreply.github.com>
2023-09-20 00:54:58 +0700

    initialize PM utils

--- src/main/InterfaceUtils/PortManagerOps/Port/PMPortUtils.java
+++ src/main/InterfaceUtils/PortManagerOps/Port/PMPortUtils.java
@@ -1,4 +1,7 @@
 package main.InterfaceUtils.PortManagerOps.Port;
 
 public class PMPortUtils {
+	public static void edit() {
+		// TODO implement edit vehicle interface
+	}
 }
--- src/main/InterfaceUtils/PortManagerOps/Vehicles/PMVehiclesUtils.java
+++ src/main/InterfaceUtils/PortManagerOps/Vehicles/PMVehiclesUtils.java
@@ -1,4 +1,7 @@
 package main.InterfaceUtils.PortManagerOps.Vehicles;
 
 public class PMVehiclesUtils {
+	public static void edit() {
+		// TODO implement edit vehicle interface
+	}
 }
