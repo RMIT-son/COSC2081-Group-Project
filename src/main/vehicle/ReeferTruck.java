7a1e935a15c1ab58b659157a90666206d05fd25d
Thân Phụng Dũng <dungthanphung@gmail.com>
2023-09-21 18:51:04 +0700

    vn

--- src/main/vehicle/ReeferTruck.java
+++ src/main/vehicle/ReeferTruck.java
@@ -3,12 +3,14 @@ package main.vehicle;
 import main.container.*;
 import main.porttrip.Port;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 
 public class ReeferTruck extends Truck implements carryAble {
-	public ReeferTruck(int tNumber, String name, double fuel, double fuelCapacity, double carryCapacity, Port currentPort, Collection<Container> containers) {
+	public ReeferTruck(int tNumber, String name, double fuel, double fuelCapacity, double carryCapacity, Port currentPort) {
 		super(tNumber, name, fuel, fuelCapacity, carryCapacity, currentPort);
+		this.containers = new ArrayList<>();
 	}
 	@Override
 	public boolean carryAble(Container container) {
